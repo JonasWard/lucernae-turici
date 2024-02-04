37490a8e078833043d9586f984c91730e49e7ec9
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-02-04 13:16:02 +0100

    increased max storeyCount

--- src/components/GeometricalInput.tsx
+++ src/components/GeometricalInput.tsx
@@ -133,7 +133,7 @@ const defaultmax = { min: 0.1, max: 3, step: 0.01, int: false };
 const defaultmin = { min: 0.1, max: 2, step: 0.01, int: false };
 const defaultperiod = { min: 10, max: 100, step: 0.1, int: false };
 const defaultphaseShift = { min: 10, max: 100, step: 0.1, int: false };
-const defaultstoryCount = { min: 1, max: 6, step: 1, int: true };
+const defaultstoryCount = { min: 1, max: 10, step: 1, int: true };
 const defaultbaseHeight = { min: 20, max: 100, step: 1, int: false };
 
 const ProcessingMethodRenderer: React.FC<{ method: ProcessingMethods; updateMethod: (newMethod: ProcessingMethods) => void }> = ({ method, updateMethod }) => {

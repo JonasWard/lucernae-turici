1d0ada3824468ba02d665c277d0533f9b6b32787
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-06-18 02:09:10 +0200

    plugins react

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import dns from 'node:dns';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     port: 3000,
   },

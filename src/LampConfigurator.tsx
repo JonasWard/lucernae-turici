4fbef9f193852455b01c6d0da2ca533049a48689
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-06-18 02:58:00 +0200

    lamp configurator 'working'

--- src/LampConfigurator.tsx
+++ src/LampConfigurator.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { DisplayType, SemanticsRenderObject } from './components/semantics/SemanticsRenderObject';
-import { dataObjectAsUrl, getDefaultObject, readingUrlAsDataObject } from './urlAsState/objectmap/versionReading';
+import { dataObjectAsUrl, getDefaultObject, getValueObjectFrom, readingUrlAsDataObject } from './urlAsState/objectmap/versionReading';
 import { updateDataEntry } from './urlAsState/objectmap/versionUpdate';
-import { parserObjects } from './urlAsState/test/semanticlyNestedDataTest';
+import { globalDataAttributeMapper, parserObjects } from './urlAsState/test/semanticlyNestedDataTest';
 import { SemanticlyNestedDataEntry } from './urlAsState/types/semanticlyNestedDataEntry';
 import { DataEntry } from './urlAsState/types/dataEntry';
 import { Button } from 'antd';
+import App from './App';
+import { GeometryBaseData } from './geometryGeneration/baseGeometry';
 
 const displayTypeMap = {
   ['extrusion']: DisplayType.POPOVER,
@@ -27,11 +29,13 @@ const tryParse = (s: string): SemanticlyNestedDataEntry => {
 
 export const LampConfigurator: React.FC = () => {
   const { stateString } = useParams();
+  const [rerender, setRerender] = useState<boolean>(false);
   const [lastURLFromData, setLastURLFromData] = useState<string>('');
 
   const updateURLFromData = (data: SemanticlyNestedDataEntry) => {
     const newUrl = dataObjectAsUrl(data, parserObjects);
     window.history.replaceState(null, 'Same Page Title', `/configurator/${newUrl}`);
+    if (lastURLFromData !== newUrl) setRerender(true);
     setLastURLFromData(newUrl);
   };
 
@@ -39,7 +43,7 @@ export const LampConfigurator: React.FC = () => {
 
   const [activeName, setActiveName] = useState<string>('');
 
-  const updateData = (dataEntry: DataEntry) => setData(updateDataEntry({ ...data }, dataEntry, parserObjects));
+  const updateData = (dataEntry: DataEntry) => setData(updateDataEntry(data, dataEntry, parserObjects));
 
   useEffect(() => {
     updateURLFromData(data);
@@ -48,22 +52,26 @@ export const LampConfigurator: React.FC = () => {
   const resetData = () => setData(getDefaultObject(parserObjects[0], 0));
 
   return (
-    <div>
-      <div>LampConfigurator</div>
-      <div>urlString string: {stateString}</div>
-      <div>lastURLFromData : {lastURLFromData}</div>
-      <Button onClick={resetData}>reset data</Button>
-      <div style={{ position: 'absolute', top: 0, right: 0, padding: 50, width: 120 }}>
-        <SemanticsRenderObject
-          semantics={data}
-          name={''}
-          updateEntry={updateData}
-          versionEnumSemantics={parserObjects[0].versionEnumSemantics}
-          activeName={activeName}
-          setActiveName={setActiveName}
-          displayTypeMap={displayTypeMap}
-        />
+    <>
+      <App
+        gBD={getValueObjectFrom(data, globalDataAttributeMapper) as unknown as GeometryBaseData}
+        rerender={rerender}
+        completedRerender={() => setRerender(false)}
+      />
+      <div>
+        <Button onClick={resetData}>reset data</Button>
+        <div style={{ position: 'absolute', top: 0, right: 0, padding: 50, width: 120 }}>
+          <SemanticsRenderObject
+            semantics={data}
+            name={''}
+            updateEntry={updateData}
+            versionEnumSemantics={parserObjects[0].versionEnumSemantics}
+            activeName={activeName}
+            setActiveName={setActiveName}
+            displayTypeMap={displayTypeMap}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };

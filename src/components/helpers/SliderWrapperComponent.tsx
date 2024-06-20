f7baf83120fe375899835570cc0232ba4a560889
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-06-20 03:18:49 +0200

    expanded slider to work with float and int defs

--- src/components/helpers/SliderWrapperComponent.tsx
+++ src/components/helpers/SliderWrapperComponent.tsx
@@ -1,17 +1,22 @@
 import { Slider } from 'antd';
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-export const SliderWrapper: React.FC<{ value: number; onChange: (value: number) => void; min: number; max: number; step: number }> = ({
-  value,
-  onChange,
-  min,
-  max,
-  step,
-}) =>
+export const SliderWrapper: React.FC<{
+  icon: ReactNode;
+  value: number;
+  onChange: (value: number) => void;
+  min: number;
+  max: number;
+  step: number;
+  precision: number;
+}> = ({ value, onChange, min, max, step, icon, precision }) =>
   value || value === 0 ? (
-    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-      <Slider style={{ width: 400 }} value={value} onChange={onChange} min={min} max={max} step={step} />
-      {value.toFixed(2)}
+    <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', width: '100%', height: 10 }}>
+        <div>{icon}</div>
+        <div>{value.toFixed(precision)}</div>
+      </div>
+      <Slider style={{ width: '100%' }} value={value} onChange={onChange} min={min} max={max} step={step} />
     </div>
   ) : (
     <span>Value is not valid</span>

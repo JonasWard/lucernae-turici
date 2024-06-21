e8aba4bbb55c831c0b0cbb456791643351d37dd9
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-06-21 12:43:45 +0200

    change ViewCube size

--- src/components/ViewCube.tsx
+++ src/components/ViewCube.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CiLocationArrow1 } from 'react-icons/ci';
 import { FaLocationArrow } from 'react-icons/fa';
 
 export enum ViewCubeSide {
@@ -20,7 +21,7 @@ interface IViewCubeProps {
   onSideChange: (cameraParameters: CameraParameters | undefined) => void;
 }
 
-export const ViewCube: React.FC<IViewCubeProps> = ({ onSideChange, size = 20 }) => {
+export const ViewCube: React.FC<IViewCubeProps> = ({ onSideChange, size = 15 }) => {
   const setSide = (side: ViewCubeSide) => {
     const target = { x: 0, y: 8, z: 0 };
     const radius = 40;
@@ -43,7 +44,7 @@ export const ViewCube: React.FC<IViewCubeProps> = ({ onSideChange, size = 20 }) 
   };
 
   return (
-    <div style={{ position: 'absolute', left: size * 1.5, bottom: size * 1.2, width: size * 2.25, height: size * 2.25 }}>
+    <div style={{ position: 'absolute', left: size * (0.5 + 2.3), bottom: size * (0.5 + 2), width: 0, height: 0 }}>
       <Arrow size={size} setSide={setSide} side={ViewCubeSide.NORTHEAST} />
       <Arrow size={size} setSide={setSide} side={ViewCubeSide.NORTHWEST} />
       <Arrow size={size} setSide={setSide} side={ViewCubeSide.SOUTHEAST} />
@@ -53,7 +54,7 @@ export const ViewCube: React.FC<IViewCubeProps> = ({ onSideChange, size = 20 }) 
 };
 
 interface IArrowProps {
-  size?: number;
+  size: number;
   setSide: (side: ViewCubeSide) => void;
   side: ViewCubeSide;
 }
@@ -61,37 +62,59 @@ interface IArrowProps {
 const getAngleForSide = (side: ViewCubeSide) => {
   switch (side) {
     case ViewCubeSide.NORTHEAST:
-      return 0;
-    case ViewCubeSide.SOUTHEAST:
-      return 0.25;
-    case ViewCubeSide.SOUTHWEST:
       return 0.5;
-    case ViewCubeSide.NORTHWEST:
+    case ViewCubeSide.SOUTHEAST:
       return 0.75;
+    case ViewCubeSide.SOUTHWEST:
+      return 1;
+    case ViewCubeSide.NORTHWEST:
+      return 1.25;
   }
 };
 
-const getStyleAlignmentForSide = (side: ViewCubeSide) => {
+const getStyleAlignmentForSide = (side: ViewCubeSide, size: number) => {
+  const arrowInset = 0;
+  const textInset = 0;
+
   switch (side) {
     case ViewCubeSide.NORTHEAST:
-      return { arrowPosition: { right: 0, top: 0 }, arrowTransform: { transform: `rotate(${getAngleForSide(side)}turn)` }, text: { left: 0, bottom: 0 } };
+      return {
+        arrowPosition: { left: arrowInset, bottom: arrowInset },
+        arrowTransform: { transform: `rotate(${getAngleForSide(side)}turn)` },
+        text: { right: textInset, top: textInset },
+      };
     case ViewCubeSide.NORTHWEST:
-      return { arrowPosition: { left: 0, top: 0 }, arrowTransform: { transform: `rotate(${getAngleForSide(side)}turn)` }, text: { right: 0, bottom: 0 } };
+      return {
+        arrowPosition: { right: arrowInset, bottom: arrowInset },
+        arrowTransform: { transform: `rotate(${getAngleForSide(side)}turn)` },
+        text: { left: textInset, top: textInset },
+      };
     case ViewCubeSide.SOUTHWEST:
-      return { arrowPosition: { left: 0, bottom: 0 }, arrowTransform: { transform: `rotate(${getAngleForSide(side)}turn)` }, text: { right: 0, top: 0 } };
+      return {
+        arrowPosition: { right: arrowInset, top: arrowInset },
+        arrowTransform: { transform: `rotate(${getAngleForSide(side)}turn)` },
+        text: { left: textInset, bottom: textInset },
+      };
     case ViewCubeSide.SOUTHEAST:
-      return { arrowPosition: { right: 0, bottom: 0 }, arrowTransform: { transform: `rotate(${getAngleForSide(side)}turn)` }, text: { left: 0, top: 0 } };
+      return {
+        arrowPosition: { left: arrowInset, top: arrowInset },
+        arrowTransform: { transform: `rotate(${getAngleForSide(side)}turn)` },
+        text: { right: textInset, bottom: textInset },
+      };
   }
 };
 
-export const Arrow: React.FC<IArrowProps> = ({ setSide, side, size = 20 }) => {
+export const Arrow: React.FC<IArrowProps> = ({ setSide, side, size }) => {
   const color = 'blue';
-  const { arrowPosition, arrowTransform, text } = getStyleAlignmentForSide(side);
+  const { arrowPosition, arrowTransform, text } = getStyleAlignmentForSide(side, size);
+
+  const height = size * 1.8;
+  const width = size * 2.1;
 
   return (
-    <div style={{ position: 'absolute', ...arrowPosition, cursor: 'pointer' }} onClick={() => setSide(side)}>
-      <FaLocationArrow size={size} fill={color} style={{ position: 'absolute', ...arrowTransform, ...arrowPosition }} />
-      <span style={{ position: 'absolute', textAnchor: 'middle', verticalAlign: 'middle', color, ...text }}>{side}</span>
+    <div style={{ position: 'absolute', ...arrowPosition, cursor: 'pointer', height, width }} onClick={() => setSide(side)}>
+      <CiLocationArrow1 size={size * 1.2} fill={color} style={{ position: 'absolute', ...arrowTransform, ...arrowPosition }} />
+      <span style={{ position: 'absolute', textAnchor: 'middle', verticalAlign: 'middle', color, ...text, fontSize: size * 0.9 }}>{side}</span>
     </div>
   );
 };

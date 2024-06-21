eb29b6cd94aadd997b8a93c9c774b58d2f9dc16b
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-06-21 12:07:04 +0200

    dev mode port 3010

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ dns.setDefaultResultOrder('verbatim');
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3010,
   },
   build: {
     outDir: './build',

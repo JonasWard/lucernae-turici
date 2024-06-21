d324973d6daa274c708a458bd7f3fd52f8b7b2b1
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-06-21 22:52:34 +0200

    global LAMP_MESH name

--- src/geometryGeneration/geometryEntry.ts
+++ src/geometryGeneration/geometryEntry.ts
@@ -22,9 +22,7 @@ export const AddLampGeometryToScene = (
   scene: Scene,
   renderMethod: RenderMethod = RenderMethod.NORMAL
 ): Mesh | TransformNode => {
-  const name = 'lampGeometry';
-
-  scene.meshes.filter((m) => m.name === name).forEach((m) => m.dispose());
+  scene.meshes.filter((m) => m.name === LAMP_MESH).forEach((m) => m.dispose());
 
   const existingRootNode = scene.getTransformNodeByName('rootNode');
   const rootNode = existingRootNode ? existingRootNode : new TransformNode('rootNode', scene);
@@ -32,16 +30,16 @@ export const AddLampGeometryToScene = (
   const voxelComplex = VoxelFactory.getVoxelComplexFromGeometryBaseData(lampGeometry);
   switch (renderMethod) {
     case RenderMethod.NORMAL:
-      return VoxelComplexMeshArtist.render(voxelComplex, scene, lampGeometry, undefined, name);
+      return VoxelComplexMeshArtist.render(voxelComplex, scene, lampGeometry, undefined, LAMP_MESH);
     case RenderMethod.WIREFRAME:
       const exisitingWireframeMaterial = scene.getMaterialByName('wireframeMaterial');
       const wireframeMaterial = exisitingWireframeMaterial ? exisitingWireframeMaterial : new StandardMaterial('wireframeMaterial', scene);
       wireframeMaterial.wireframe = true;
-      return VoxelComplexMeshArtist.render(voxelComplex, scene, lampGeometry, wireframeMaterial, name);
+      return VoxelComplexMeshArtist.render(voxelComplex, scene, lampGeometry, wireframeMaterial, LAMP_MESH);
     case RenderMethod.ENCLOSURE:
     case RenderMethod.HALFEDGESENCLOSURE:
       const enclosureMesh = getHalfEdgeMeshForVoxelEnclosure(voxelComplex);
-      if (renderMethod === RenderMethod.ENCLOSURE) return HalfEdgeMeshRenderer.render(enclosureMesh, scene, undefined, name);
+      if (renderMethod === RenderMethod.ENCLOSURE) return HalfEdgeMeshRenderer.render(enclosureMesh, scene, undefined, LAMP_MESH);
       Object.values(enclosureMesh.halfEdges).map((he) => renderHalfEdge(he, enclosureMesh, scene, undefined, rootNode));
       return rootNode;
     case RenderMethod.NEIGHHBOURMAP:

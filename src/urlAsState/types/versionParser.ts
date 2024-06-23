82ceb4d323bae7f88bbb8891500e4cad71c22204
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-06-23 22:40:18 +0200

    new objectGenerator definitions using array

--- src/urlAsState/types/versionParser.ts
+++ src/urlAsState/types/versionParser.ts
@@ -1,7 +1,13 @@
+import { ObjectGenerationOutputStatus } from '../enums/objectGenerationTypes';
 import { SemanticlyNestedDataEntry } from './semanticlyNestedDataEntry';
 import { GlobalVersion } from './versionData';
 
-export type ObjectGeneratorMethod = (...v: (number | boolean | undefined)[]) => SemanticlyNestedDataEntry;
+/**
+ * A method that generates a nested object based on a set of values
+ * @param v - The values to be used to generate the object, can be either a valid value for a dataentry, a bitstring (only 0 or 1 chars) or undefined (default value)
+ * @returns [The generated object, the generation status, the index end bit of the bit url (-1 if)]
+ */
+export type ObjectGeneratorMethod = (...v: (number | boolean | string | undefined)[]) => [SemanticlyNestedDataEntry, ObjectGenerationOutputStatus, number];
 
 export type VersionEnumSemantics = {
   [key: string]: { value: number; label: string }[];
@@ -12,7 +18,7 @@ export type ParserForVersion = {
   versionName: string;
   versionEnumSemantics?: VersionEnumSemantics;
   versionValueAttributeMapper?: { [key: string]: string };
-  objectGenerator: ObjectGeneratorMethod;
+  objectGenerator: ObjectGeneratorMethod[];
 };
 
 export type VersionParsers = {

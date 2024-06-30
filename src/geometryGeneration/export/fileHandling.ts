3e47cd06d108a603ff214b7e4d0960747b57ebff
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-06-30 12:45:05 +0200

    fixed double export of stls

--- src/geometryGeneration/export/fileHandling.ts
+++ src/geometryGeneration/export/fileHandling.ts
@@ -13,21 +13,9 @@ export const exportOBJ = (scene: Scene, fileName = 'lamp-geometry') => {
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
-
-  return scene;
 };
 
 export const exportSTL = (scene: Scene, fileName = 'lamp-geometry') => {
   const mesh = scene.getMeshByName(LAMP_MESH);
-  const data = STLExport.CreateSTL([mesh]);
-
-  const a = document.createElement('a');
-  a.href = window.URL.createObjectURL(new Blob([data], { type: 'text/plain' }));
-  a.download = `${fileName}.stl`;
-
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-
-  return scene;
+  STLExport.CreateSTL([mesh], true, `${fileName}`);
 };

a4286af61aae7ed38e1ceaa002a25f9c888b9511
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-03 20:30:01 +0200

    clean up

--- src/geometryGeneration/geometry.ts
+++ src/geometryGeneration/geometry.ts
@@ -6,18 +6,6 @@ export enum Malculmiuses {
   One = 5,
 }
 
-export type MalculmiusOneGeometry = {
-  type: Malculmiuses.One;
-  circleRadius: number;
-  circleDivisions: number;
-  angleSplit: number;
-  offsetA: number;
-  offsetB: number;
-  innerRadius: number;
-  postProcessing: PostProcessingMethods;
-  heights: HeightGenerator;
-};
-
 export enum ProcessingMethodType {
   None,
   IncrementalMethod,
@@ -148,81 +136,6 @@ export const getHeights = (heightGenerator: HeightGenerator): number[] => {
 };
 
 export type MalculmiusGeometryType = Malculmiuses.One;
-
-export type MalculmiusGeometry = MalculmiusOneGeometry;
-
-const MALCULMIUS_TOLERANCE = 0.01;
-
-const copyAndRotate = (vss: Vector3[][], origin: Vector3, angle: number): Vector3[][] => {
-  const cos = Math.cos(angle);
-  const sin = Math.sin(angle);
-
-  return vss.map((vs) =>
-    vs.map((v) => new Vector3((v.x - origin.x) * cos - (v.y - origin.y) * sin + origin.x, (v.x - origin.x) * sin + (v.y - origin.y) * cos + origin.y, v.z))
-  );
-};
-
-export const createShardOfMalculmiusOne = (geometry: MalculmiusOneFootprint, origin: Vector3, angle: number): Vector3[][] => {
-  // creating the base profiles
-  const singleShard = (o: Vector3, v0: Vector3, v1: Vector3, r0: number, r1: number, o0: number): Vector3[][] => {
-    const t0 = o.add(v0.scale(r1));
-    const t1 = o.add(v1.scale(r1));
-
-    const topVs: Vector3[] = [];
-
-    if (Math.abs(o0) < MALCULMIUS_TOLERANCE) {
-      topVs.push(t0, t1);
-    } else {
-      const d = t0.add(t1).scale(0.5);
-      topVs.push(t0, o.add(d.scale(1 + o0 / d.length())), t1);
-    }
-
-    if (r0 < MALCULMIUS_TOLERANCE) {
-      if (topVs.length === 3 && o0 < 0) {
-        return [
-          [o, topVs[0], topVs[1]],
-          [o, topVs[1], topVs[2]],
-        ];
-      } else {
-        return [[o, topVs[0], topVs[1], topVs[2]]];
-      }
-    } else {
-      const p0 = o.add(v0.scale(r0));
-      const p1 = o.add(v1.scale(r0));
-
-      if (topVs.length === 3 && o0 < 0) {
-        return [
-          [p1, p0, topVs[1]],
-          [topVs[1], p0, topVs[0]],
-          [topVs[2], p1, topVs[1]],
-        ];
-      } else if (topVs.length === 3) {
-        return [[p1, p0, ...topVs]];
-      } else {
-        return [[p1, p0, topVs[0], topVs[1]]];
-      }
-    }
-  };
-
-  const { circleRadius, circleDivisions, angleSplit, offsetA, offsetB, innerRadius } = geometry;
-
-  const vertexSets: Vector3[][] = [];
-
-  const angleDelta = (Math.PI * 2) / circleDivisions;
-  const angleDeltaSplit = angleDelta * angleSplit;
-
-  const v0 = new Vector3(1, 0, 0);
-  const v1 = new Vector3(Math.cos(angleDeltaSplit), Math.sin(angleDeltaSplit), 0);
-  const v2 = new Vector3(Math.cos(angleDelta), Math.sin(angleDelta), 0);
-
-  const shards = [...singleShard(origin, v0, v1, innerRadius, circleRadius, offsetA), ...singleShard(origin, v1, v2, innerRadius, circleRadius, offsetB)];
-
-  for (let i = 0; i < circleDivisions; i++) {
-    vertexSets.push(...copyAndRotate(shards, origin, i * angleDelta));
-  }
-
-  return vertexSets;
-};
 
 export const twistAndSkewVertex = (v: Vector3, twistMethod: (angle: number) => number, skewMethod: (angle: number) => number, angle: number): Vector3 => {
   const twistAngle = twistMethod(v.z * 0.001);

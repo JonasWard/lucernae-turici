7f4e76d59f52831846221fe93e912853ecd8ac6e
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-05 11:50:52 +0200

    remove old

--- src/old/dataSemanticsEnums.ts
+++
@@ -1,6 +0,0 @@
-export enum SemanticValueString {
-  version = 'version',
-  extrusionTypeParameters = 'extrusionTypeParameters',
-  floorTypeParameters = 'floorTypeParameters',
-  heightMethodParameters = 'heightMethodParameters',
-}

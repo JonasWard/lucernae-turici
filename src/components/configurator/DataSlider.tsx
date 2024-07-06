f4c49b8cad1bd30ce6c12857b41b66a987b3eb78
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-07 00:12:53 +0200

    implemente lazy slider

--- src/components/configurator/DataSlider.tsx
+++ src/components/configurator/DataSlider.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getVersionDataForLerping, lerpData } from '../../urlAsState/utils/spline';
+import { SemanticlyNestedDataEntry } from '../../urlAsState/types/semanticlyNestedDataEntry';
+import { Slider } from 'antd';
 
 interface IDataSliderProps {
-  urls: string[];
-  setData: (value: number) => void;
+  version: number;
+  setData: (value: SemanticlyNestedDataEntry) => void;
 }
 
-export const DataSlider: React.FC<IDataSliderProps> = ({ urls, setData }) => {
-  return <div>SLider</div>;
+export const DataSlider: React.FC<IDataSliderProps> = ({ version, setData }) => {
+  const [localVersion, setLocalVersion] = useState(version);
+  const [baseData, setBaseData] = useState(getVersionDataForLerping(version));
+  const [t, setT] = useState(0.5);
+
+  useEffect(() => {
+    if (localVersion === version) return;
+    setBaseData(getVersionDataForLerping(version));
+    setLocalVersion(version);
+  }, [version]);
+
+  useEffect(() => {
+    const { baseDataArray, keyDataArray } = baseData;
+    setData(lerpData(t, baseDataArray, keyDataArray, version));
+  }, [t, localVersion]);
+
+  return (
+    <div style={{ position: 'absolute', left: 50, top: 50, bottom: 130 }}>
+      <Slider min={0} max={1} step={0.001} vertical value={t} onChange={(t) => setT(t)}></Slider>
+    </div>
+  );
 };

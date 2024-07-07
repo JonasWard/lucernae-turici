5423405bcfa341e43e4c50d812ced983147b472a
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-07-07 15:43:08 +0200

    dataSlider doesn't automatically set the data

--- src/components/configurator/DataSlider.tsx
+++ src/components/configurator/DataSlider.tsx
@@ -11,7 +11,6 @@ interface IDataSliderProps {
 export const DataSlider: React.FC<IDataSliderProps> = ({ version, setData }) => {
   const [localVersion, setLocalVersion] = useState(version);
   const [baseData, setBaseData] = useState(getVersionDataForLerping(version));
-  const [t, setT] = useState(0.5);
 
   useEffect(() => {
     if (localVersion === version) return;
@@ -19,14 +18,11 @@ export const DataSlider: React.FC<IDataSliderProps> = ({ version, setData }) => 
     setLocalVersion(version);
   }, [version]);
 
-  useEffect(() => {
-    const { baseDataArray, keyDataArray } = baseData;
-    setData(lerpData(t, baseDataArray, keyDataArray, version));
-  }, [t, localVersion]);
+  const setT = (t: number) => setData(lerpData(t, baseData.baseDataArray, baseData.keyDataArray, version));
 
   return (
-    <div style={{ position: 'absolute', left: 50, top: 50, bottom: 130 }}>
-      <Slider min={0} max={1} step={0.001} vertical value={t} onChange={(t) => setT(t)}></Slider>
+    <div style={{ position: 'absolute', left: 10, top: 130, bottom: 130 }}>
+      <Slider min={0} max={1} step={0.001} vertical onChange={(t) => setT(t)}></Slider>
     </div>
   );
 };
